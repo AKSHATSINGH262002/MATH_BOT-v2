58cc809cf4f40ec48c4a2685d7eef8b7f278d2b9
Akshat singh <157829533+AKSHATSINGH262002@users.noreply.github.com>
2025-03-25 11:08:47 +0530

    Update pre.py

--- pre.py
+++ pre.py
@@ -8,7 +8,7 @@ import io
 
 
 
-pytesseract.tesseract_cmd=r'tesseract-ocr-w64-setup-5.5.0.20241111.exe'
+pytesseract.tesseract_cmd=r'git@github.com:AKSHATSINGH262002/MATH_BOT-v2.git'
 def extract(image_path):
   #image=Image.open(image_path)
    text=pytesseract.image_to_string(image_path)
